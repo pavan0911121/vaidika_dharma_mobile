1877e0797b16383e47753a2f5ed807b8fd965cef
pavan0911121 <pavankarthik0911@gmail.com>
2025-08-14 01:17:32 +0530

    added react navigation

--- App.tsx
+++ App.tsx
@@ -1,41 +1,24 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
-
 
 function HomeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View>
-        <Text>Home Screen</Text>
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
 
-function RootStack() {
-  
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </SafeAreaView>
-  );
-}
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  
-  return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
